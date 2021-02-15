b5f5c862563c2218bbc9ca200081cbca4e2777cc
fynaghk <n.fidan_15@mail.ru>
2021-02-15 19:15:02 +0400

    Done!

--- Colorful-box/index.js
+++ Colorful-box/index.js
@@ -9,20 +9,55 @@ const again = false;
 
 redBtn.addEventListener("click", ()=>{
   box.style.background="red";
+  redBtn.addEventListener("click", ()=>{
+    if(box.style.background=="red"){
+      box.style.background="white";
+    }else{
+      box.style.background="red";
+    }
+  })
 })
 
 greenBtn.addEventListener("click", ()=>{
   box.style.background="green";
+  greenBtn.addEventListener("click", ()=>{
+    if(box.style.background=="green"){
+      box.style.background="white";
+    }else{
+      box.style.background="green";
+    }
+  })
 })
 
 blackBtn.addEventListener("click",  ()=>{
   box.style.background="black";
+  blackBtn.addEventListener("click", ()=>{
+    if(box.style.background=="black"){
+      box.style.background="white";
+    }else{
+      box.style.background="black";
+    }
+  })
 })
 
 blueBtn.addEventListener("click",  ()=>{
   box.style.background="blue";
+  blueBtn.addEventListener("click", ()=>{
+    if(box.style.background=="blue"){
+      box.style.background="white";
+    }else{
+      box.style.background="blue";
+    }
+  })
 })
 
 yellowBtn.addEventListener("click",  ()=>{
   box.style.background="yellow";
+  yellowBtn.addEventListener("click", ()=>{
+    if(box.style.background=="yellow"){
+      box.style.background="white";
+    }else{
+      box.style.background="yellow";
+    }
+  })
 })
