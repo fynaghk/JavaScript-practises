98795b06f4b85194fe2a9663700253b5c8c5e1d2
fynaghk <n.fidan_15@mail.ru>
2021-02-18 20:01:58 +0400

    Ongoing...

--- Testimonials/index.js
+++ Testimonials/index.js
@@ -1,53 +1,24 @@
 const leftBtn = document.querySelector(".fa-arrow-alt-circle-left");
 const rightBtn = document.querySelector(".fa-arrow-alt-circle-right");
-const img = document.querySelector("figure");
-const comment = document.querySelector("p");
-const star = document.querySelector(".icons");
+const myDiv = document.querySelector("#all");
 
 leftBtn.addEventListener("click", moveToLeft);
 rightBtn.addEventListener("click", moveToRight);
 
 function moveToLeft(){
-  img.animate([
+  myDiv.animate([
      {transform: 'translateX(0px)'},
-     {transform: 'translateX(-600px)'}
+     {transform: 'translateX(-750px)'},
   ],  {
     duration: 1000,
   })
 
-  star.animate([
-    {transform: 'translateX(0px)'},
-    {transform: 'translateX(-600px)'}
- ],  {
-   duration: 1000,
- })
-
-  comment.animate([
-    {transform: 'translateX(0px)'},
-    {transform: 'translateX(-600px)'}
- ],  {
-   duration: 1000,
- })
 }
 
 function moveToRight(){
-  img.animate([
-     {transform: 'translateX(0px)'},
-     {transform: 'translateX(600px)'}
-  ],  {
-    duration: 1000,
-  })
-
-  star.animate([
+  myDiv.animate([
     {transform: 'translateX(0px)'},
-    {transform: 'translateX(600px)'}
- ],  {
-   duration: 1000,
- })
-
-  comment.animate([
-    {transform: 'translateX(0px)'},
-    {transform: 'translateX(600px)'}
+    {transform: 'translateX(750px)'},
  ],  {
    duration: 1000,
  })
