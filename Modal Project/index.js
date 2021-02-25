f2e13972f5056bb8646b1999f9393daae80e3da6
fynaghk <n.fidan_15@mail.ru>
2021-02-25 18:38:42 +0400

    Done!

--- Modal Project/index.js
+++ Modal Project/index.js
@@ -1,4 +1,4 @@
-const box = document.querySelector(".box");
+const box = document.querySelectorAll(".box");
 const modalImage = document.querySelector(".modalImage");
 const rightBtn = document.querySelector(".fa-arrow-circle-right");
 const leftBtn = document.querySelector(".fa-arrow-circle-left");
@@ -13,11 +13,23 @@ const imageArray = [
   "url('./images/Sherlock6.png')"
 ]
 
-modalImage.style.background = imageArray[0];
+modalImage.style.background = imageArray[i];
 
 rightBtn.addEventListener("click", rightBtnFunction);
 
 leftBtn.addEventListener("click", leftBtnFunction);
+
+box[0].addEventListener("click", selectImage0);
+
+box[1].addEventListener("click", selectImage1);
+
+box[2].addEventListener("click", selectImage2);
+
+box[3].addEventListener("click", selectImage3);
+
+box[4].addEventListener("click", selectImage4);
+
+box[5].addEventListener("click", selectImage5);
 
 
 
@@ -44,6 +56,33 @@ function leftBtnFunction(){
   }
 }
 
-function selectImage(){
+function selectImage0(){
+  i = 0;
+  modalImage.style.background = imageArray[i];
+  console.log("efjk");
+}
 
+function selectImage1(){
+  i = 1;
+  modalImage.style.background = imageArray[i];
+}
+
+function selectImage2(){
+  i = 2;
+  modalImage.style.background = imageArray[i];
+}
+
+function selectImage3(){
+  i = 3;
+  modalImage.style.background = imageArray[i];
+}
+
+function selectImage4(){
+  i = 4;
+  modalImage.style.background = imageArray[i];
+}
+
+function selectImage5(){
+  i = 5;
+  modalImage.style.background = imageArray[i];
 }
