588ebe3fbd1fdd7b582eb43f2c07447b28f5cb1b
fynaghk <n.fidan_15@mail.ru>
2021-02-26 20:53:39 +0400

    Ongoing...

--- Calculator/index.js
+++ Calculator/index.js
@@ -23,4 +23,81 @@ const input = document.querySelector("#screen"),
        dotBtn = document.querySelector("#dot");
 
 const keys = document.querySelectorAll("button");
+let operation = "";
+
+// AddEventListeners section starts there
+
+oneBtn.addEventListener("click", addOne);
+twoBtn.addEventListener("click", addTwo);
+threeBtn.addEventListener("click", addThree);
+fourBtn.addEventListener("click", addFour);
+fiveBtn.addEventListener("click", addFive);
+sixBtn.addEventListener("click", addSix);
+sevenBtn.addEventListener("click", addSeven);
+eightBtn.addEventListener("click", addEight);
+nineBtn.addEventListener("click", addNine);
+zeroBtn.addEventListener("click", addZero);
+doubleZeroBtn.addEventListener("click", addDoubleZero);
+
+// My Functions start there
+
+function addOne(){
+       operation += "1";
+       input.innerHTML = operation;
+}
+
+function addTwo(){
+       operation += "2";
+       input.innerHTML = operation;
+}
+
+function addThree(){
+       operation += "3";
+       input.innerHTML = operation;
+}
+
+function addFour(){
+       operation += "4";
+       input.innerHTML = operation;
+}
+
+function addFive(){
+       operation += "5";
+       input.innerHTML = operation;
+}
+
+function addSix(){
+       operation += "6";
+       input.innerHTML = operation;
+}
+
+function addSeven(){
+       operation += "7";
+       input.innerHTML = operation;
+}
+
+function addEight(){
+       operation += "8";
+       input.innerHTML = operation;
+}
+
+function addNine(){
+       operation += "9";
+       input.innerHTML = operation;
+}
+
+function addSeven(){
+       operation += "7";
+       input.innerHTML = operation;
+}
+
+function addZero(){
+       operation += "0";
+       input.innerHTML = operation;
+}
+
+function addDoubleZero(){
+       operation += "00";
+       input.innerHTML = operation;
+}
 
